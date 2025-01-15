24bab2461333559fcff63f4b645400ba19818264
mouadrguibi <146367397+mouad4949@users.noreply.github.com>
2025-01-15 05:01:24 +0100

    Update UsersMicroserviceApplicationTests.java

--- src/test/java/com/banque/users_microservice/UsersMicroserviceApplicationTests.java
+++ src/test/java/com/banque/users_microservice/UsersMicroserviceApplicationTests.java
@@ -2,11 +2,12 @@ package com.banque.users_microservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
 @TestPropertySource(properties = {
-    "notification-service.url=http://localhost:8085",
-    "spring.kafka.enabled=false"
+        "notification-service.url=http://localhost:8085",
+        "spring.kafka.enabled=false"
 })
 class UsersMicroserviceApplicationTests {
 
